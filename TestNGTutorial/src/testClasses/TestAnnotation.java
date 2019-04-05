e872e9964be5b9b952ad76067f5611dd7cd474fd
Aman Sinha <aman.sinha42@gmail.com>
2019-04-05 14:54:01 +0530

    working with test annotation

--- TestNGTutorial/src/testClasses/TestAnnotation.java
+++ TestNGTutorial/src/testClasses/TestAnnotation.java
@@ -1,10 +1,33 @@
 package testClasses;
 
 import org.testng.annotations.Test;
-import org.testng.annotations.Test;
+
+import appcode.SomeClassToTest;
+//import org.testng.annotations.Test;
+
 
 public class TestAnnotation {
-  @Test
-  public void f() {
-  }
+
+	@Test
+	// method to annotate test method
+	public void testMethod1() {
+		SomeClassToTest obj = new SomeClassToTest();
+		int result = obj.sumNumber(4, 7);
+		System.out.println(result);
+		System.out.println("Running test -> testMethod1");
+		
+	}
+	@Test
+	// method to annotate test method
+	public void testMethod2() {
+		System.out.println("Running test -> testMethod2");
+		
+	}
+	@Test
+	// method to annotate test method
+	public void testMethod3() {
+		System.out.println("Running test -> testMethod3");
+		
+	}	
+	//Default order to run basis of method name (alphabetically)
 }
