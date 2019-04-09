fce0156174c1cb4370ffb34d0fcc7b4a250405ff
Aman Sinha <aman.sinha42@gmail.com>
2019-04-10 00:38:23 +0530

    Seperating data provider and test classes

---
+++ TestNGTutorial/src/testClasses/TestData.java
@@ -0,0 +1,14 @@
+package testClasses;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class TestData {
+
+	@DataProvider(name="inputs")
+	public Object[][] getData() {
+		return new Object[][] { { "bmw", "m3" }, { "audi", "a6" }, { "benz", "c300" } };
+	}
+
+	
+}
--- TestNGTutorial/src/testClasses/TestNG_DataProviders.java
+++ TestNGTutorial/src/testClasses/TestNG_DataProviders.java
@@ -1,16 +1,16 @@
 package testClasses;
 
-import org.testng.annotations.DataProvider;
+//import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TestNG_DataProviders {
 	
-	@DataProvider(name="inputs")
-	public Object[][] getData() {
-		return new Object[][] { { "bmw", "m3" }, { "audi", "a6" }, { "benz", "c300" } };
-	}
+	//@DataProvider(name="inputs")
+	//public Object[][] getData() {
+		//return new Object[][] { { "bmw", "m3" }, { "audi", "a6" }, { "benz", "c300" } };
+	//}
 
-	@Test(dataProvider="inputs")
+	@Test(dataProvider="inputs",dataProviderClass=TestData.class)//Location of data provider class
 	public void testMethod1(String input1, String input2) {
 		System.out.println("Input 1: " + input1);
 		System.out.println("Input 2: " + input2);
